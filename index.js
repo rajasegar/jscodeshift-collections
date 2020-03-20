d880af4930e4d31fced820144db13bf4684a82dc
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-03-20 08:25:07 +0530

    chore: publish to npm
    1. add release-it
    2. add release script to package.json
    3. export collections and register method

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
-const Collection = require('jscodeshift/src/Collection');
-const recast = require('recast');
-
-
-const nodes = [recast.parse(
-  'function foo() {}',
-  'function bar(a,b,c) {}',
-)];
-
-console.log(recast.print(nodes[0]));
 const FunctionDeclarationCollection = require('./collections/FunctionDeclaration');
+const CallExpressionCollection = require('./collections/CallExpression');
 
-FunctionDeclarationCollection.register();
+function registerCollections() {
+  FunctionDeclarationCollection.register();
+}
 
-
-const declarators = Collection.fromNodes(nodes).findFunctionDeclarations();
-
-console.log('Length', declarators.length);
-
-Collection.fromNodes(nodes)
-  .findFunctionDeclarations('foo')
-  .renameTo('xyz');
-
-
-console.log(recast.print(nodes[0]));
+module.exports = {
+  registerCollections,
+  FunctionDeclarationCollection,
+  CallExpressionCollection,
+};
