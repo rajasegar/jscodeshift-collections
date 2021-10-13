c708512e540c8978746d763c17fdd97ade15a480
Rajasegar <rajasegar.c@gmail.com>
2021-10-13 11:41:27 +0530

    chore: fix lint errors in eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,14 @@ module.exports = {
   env: {
     node: true,
   },
-  plugins: ["prettier", "node"],
-  extends: ["eslint:recommended", "plugin:node/recommended", "prettier"],
+  plugins: ['prettier', 'node'],
+  extends: ['eslint:recommended', 'plugin:node/recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: 'module',
   },
   rules: {
-    "prettier/prettier": "error",
-    "node/no-unpublished-require": 0,
+    'prettier/prettier': 'error',
+    'node/no-unpublished-require': 0,
   },
 };
